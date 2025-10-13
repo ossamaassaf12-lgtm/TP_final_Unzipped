8023dceb5aacc8eb0e837c61d57fb6cf7d7b9778
OSSAMA <ossama.assaf12@gmail.com>
2025-10-13 16:32:11 +0200

    Update creation_etape_1.sh

--- creation_etape_1.sh
+++ creation_etape_1.sh
@@ -27,38 +27,6 @@ echo "David:A405012" | sudo chpasswd
 mkdir Challange
 chmod 777 Challange
 
-cd Challange
-
-#creation de dossier administrators
-
-
-mkdir administrators
-chmod 700 administrators
-cd administrators
-echo 'ÏÏÿÿÿÿÿÿÿÿBiff8 ÿÿÿÿÿÿÿÿÿÿÿÿÿÿ ÿÿÿÿ ÿÿÿÿÿÿÿÿÿÿ ÿÿÿÿ
-Workbook   .........................._VBA_PROJECT_CUR  ..........data.....     ........
-..Þ.._...É...f..d.C.B...R...k...#..ë...........biff8...Excel.Sheet.8.............
-................... ÿÿÿÿÿÿÿÿÿÿ...ÿÿ..œ.... ƒ.„.›...†.Ú.......................
-
-nameÏÏÿÿÿDepartmentÏÏÿÿÿOTP
-ÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿ
-BobÏÏÿÿÿadminstratorÏÏÿÿÿ1245678
-CharlieÏÏÿÿÿadminstratorÏÏÿÿÿ1223334444
-ÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿ
-ÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿ
-ÏÏÿÿÿ
-ÏÏÿÿÿ
-ÏÏÿÿÿ
-ÏÏÿÿÿ
-ÏÏÿÿÿ
-ÏÏÿÿÿ
-ÏÏÿÿÿ
-ÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿÏÏÿÿÿ
-ÏÏÿÿÿ ' > administrators.xls
-
-
-chmod 755 administrators.xls
- 
 
 
 
